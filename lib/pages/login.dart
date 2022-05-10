9de3031241ddcfa31b0a90389a2b79586c2a11d1
Story5 <337995545@qq.com>
2022-05-11 02:40:08 +0800

    处理焦点

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,81 +9,95 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      debugDumpFocusTree();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight: MediaQuery.of(context).size.height,
-          ),
-          child: Center(
-            child: Column(
-              children: [
-                Expanded(
-                  child: Center(
-                    child: FractionallySizedBox(
-                      widthFactor: 0.4,
-                      heightFactor: 0.4,
-                      child: Image.asset('assets/images/mark.png'),
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height,
+            ),
+            child: Center(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: FractionallySizedBox(
+                        widthFactor: 0.4,
+                        heightFactor: 0.4,
+                        child: Image.asset('assets/images/mark.png'),
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24, right: 24, bottom: 12),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: const [
-                            TextField(
-                              decoration: InputDecoration(
-                                hintText: '请输入邮箱',
-                                labelText: '邮箱',
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 24, right: 24, bottom: 12),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: const [
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: '请输入邮箱',
+                                  labelText: '邮箱',
+                                ),
+                                textInputAction: TextInputAction.next,
                               ),
+                              TextField(
+                                decoration: InputDecoration(
+                                  hintText: '请输入六位以上的密码',
+                                  labelText: '密码',
+                                ),
+                                obscureText: true,
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 24, right: 24, top: 12, bottom: 12),
+                          child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(69, 202, 181, 11)),
                             ),
-                            TextField(
-                              decoration: InputDecoration(
-                                hintText: '请输入六位以上的密码',
-                                labelText: '密码',
+                            child: const Text(
+                              '登录按钮',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 24, right: 24, top: 12, bottom: 12),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text('没有账号? '),
+                              InkWell(
+                                child: const Text('立即注册'),
+                                onTap: () {},
                               ),
-                              obscureText: true,
-                            )
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24, right: 24, top: 12, bottom: 12),
-                        child: TextButton(
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(69, 202, 181, 11)),
+                            ],
                           ),
-                          child: const Text(
-                            '登录按钮',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onPressed: () {},
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 24, right: 24, top: 12, bottom: 12),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text('没有账号? '),
-                            InkWell(
-                              child: const Text('立即注册'),
-                              onTap: () {},
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
