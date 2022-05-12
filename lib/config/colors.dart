9b6847e5cb1a71391caf3fd624d15d293a1262bf
Story5 <337995545@qq.com>
2022-05-12 22:29:10 +0800

    创建BottomNavigationBar

---
+++ lib/config/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color activeTabIconColor = Color(0xff50D2C2);
+const Color inactiveTabIconColor = Colors.black;
--- lib/pages/todo_entry.dart
+++ lib/pages/todo_entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/config/colors.dart';
 
 class TodoEntryPage extends StatefulWidget {
   const TodoEntryPage({Key? key}) : super(key: key);
@@ -8,11 +9,58 @@ class TodoEntryPage extends StatefulWidget {
 }
 
 class _TodoEntryPageState extends State<TodoEntryPage> {
+  BottomNavigationBarItem _buildBottomNavigationBarItem(
+    String imagePath, {
+    double? size,
+    bool singleImage = false,
+  }) {
+    if (singleImage) {
+      return BottomNavigationBarItem(
+        icon: Image(
+          width: size,
+          height: size,
+          image: AssetImage(imagePath),
+        ),
+        label: '',
+      );
+    }
+    ImageIcon activeIcon = ImageIcon(
+      AssetImage(imagePath),
+      size: size,
+      color: activeTabIconColor,
+    );
+    ImageIcon inactiveImageIcon = ImageIcon(
+      AssetImage(imagePath),
+      size: size,
+      color: inactiveTabIconColor,
+    );
+    return BottomNavigationBarItem(
+      icon: inactiveImageIcon,
+      activeIcon: activeIcon,
+      label: '',
+    );
+  }
+
+  void _onTabChange(int index) {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('入口'),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTabChange,
+        currentIndex: 0,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          _buildBottomNavigationBarItem('assets/images/lists.png'),
+          _buildBottomNavigationBarItem('assets/images/calendar.png'),
+          _buildBottomNavigationBarItem(
+            'assets/images/add.png',
+            size: 50,
+            singleImage: true,
+          ),
+          _buildBottomNavigationBarItem('assets/images/report.png'),
+          _buildBottomNavigationBarItem('assets/images/about.png'),
+        ],
       ),
       body: Center(
         child: Text(
