ce1dad7ac318c6cf16a704fa023992479ca05458
Story5 <337995545@qq.com>
2022-05-14 00:08:46 +0800

    11.1.2 封装带有标题的LabelGroup组件

---
+++ lib/component/label_group.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LabelGroup extends StatelessWidget {
+  final String labelText;
+  final TextStyle? labelStyle;
+  final Widget child;
+  final EdgeInsetsGeometry? padding;
+
+  const LabelGroup({
+    Key? key,
+    required this.labelText,
+    this.labelStyle,
+    required this.child,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(labelText, style: labelStyle ?? Theme.of(context).inputDecorationTheme?.labelStyle),
+          child,
+        ],
+      ),
+    );
+  }
+}
