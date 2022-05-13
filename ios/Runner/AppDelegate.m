a9b48d067243673cce80c00ff0f539407d2ff1c8
Story5 <337995545@qq.com>
2022-05-14 05:37:57 +0800

    12.3.4 使用PlatformChannel编写iOS端代码

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,10 +4,24 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+        didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	FlutterViewController *controller = (FlutterViewController *)self.window.rootViewController;
+	FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"com.Story5.todo.channel" binaryMessenger:controller.binaryMessenger];
+    [channel setMethodCallHandler:^(FlutterMethodCall *call,FlutterResult result){
+        if([call.method isEqualToString:@"getCurrentLocation"]){
+            result(@{
+            @"latitude":@"39.92",
+            @"longitude":@"116.46",
+            @"description":@"北京",
+            });
+        } else {
+            result(FlutterMethodNotImplemented);
+        }
+    }];
+	[GeneratedPluginRegistrant registerWithRegistry:self];
+	// 可以在这里添加更多代码,定制应用启动后的行为
+	// Override point for customization after application launch.
+	return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
