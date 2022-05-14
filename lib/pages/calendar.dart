9d6d709a0ed1aeff42cb1f522464fbd9c01b6fbb
Story5 <337995545@qq.com>
2022-05-14 21:16:31 +0800

    table_calendar

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:todo/extension/date_time.dart';
 import 'package:todo/model/todo_list.dart';
 
 class CalendarPage extends StatefulWidget {
@@ -14,16 +16,31 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  late TodoList _todoList;
+  late DateTime _initialDay;
+
+  @override
+  void initState() {
+    super.initState();
+    _todoList = widget.todoList;
+    _initialDay = DateTime.now().dayTime;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('日历'),
       ),
-      body: Center(
-        child: Text(
-          runtimeType.toString(),
-        ),
+      body: Column(
+        children: [
+          TableCalendar(
+            focusedDay: _initialDay,
+            firstDay: DateTime(1900),
+            lastDay: DateTime(2050),
+          ),
+        ],
       ),
     );
   }
