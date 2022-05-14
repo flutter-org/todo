50fd3e84f04e4777d752461768ad7fa689cbfa65
Story5 <337995545@qq.com>
2022-05-15 02:47:15 +0800

    Create assets_res.dart

---
+++ lib/res/assets_res.dart
@@ -0,0 +1,18 @@
+/// Generated file. Do not edit.
+
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+class AssetsRes {
+  static const String mark = 'assets/images/mark.png';
+  static const String default_avatar = 'assets/images/default_avatar.png';
+  static const String lists = 'assets/images/lists.png';
+  static const String calendar = 'assets/images/calendar.png';
+  static const String add = 'assets/images/add.png';
+  static const String report = 'assets/images/report.png';
+  static const String about = 'assets/images/about.png';
+  static const String rect_selected = 'assets/images/rect_selected.png';
+  static const String rect = 'assets/images/rect.png';
+  static const String star = 'assets/images/star.png';
+  static const String star_normal = 'assets/images/star_normal.png';
+  static const String group = 'assets/images/group.png';
+}
